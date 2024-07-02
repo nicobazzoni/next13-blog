816f0e8eb06cc5a430e5b8af37cd9e217598f2e7
Nicholas Bazzoni <nico@Nicholass-MBP.fios-router.home>
2024-07-01 22:01:37 -0400

    new config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  
   reactStrictMode: true,
-  experimental: { 
-    appDir: true 
-  },
-
-  
   images: {
     domains: ['links.papareact.com', 'cdn.sanity.io'],
   },
-
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-
-  
-}
+};
